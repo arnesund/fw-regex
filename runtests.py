cc2d0d86c38fff0d90f6b076c857fc47f96946b0
Arne Sund <arnesund@users.noreply.github.com>
2015-03-16 13:44:13 +0000

    Also test get_timestamp()

--- runtests.py
+++ runtests.py
@@ -17,12 +17,24 @@ filenames = glob('test/*.log')
 
 for filename in filenames:
     for line in open(filename):
-        res = libfwregex.get_builtconn(line)
+        # Test get_timestamp()
+        res = libfwregex.get_timestamp(line)
         if not res:
-            print('TEST FAILED for line:')
+            print('get_timestamp: TEST FAILED for line:')
             print(line)
         else:
-            print('Test OK')
+            print('get_timestamp: Test OK')
+            if VERBOSE:
+                pprint(res)
+                print('')
+
+        # Test get_builtconn()
+        res = libfwregex.get_builtconn(line)
+        if not res:
+            print('get_builtconn: TEST FAILED for line:')
+            print(line)
+        else:
+            print('get_builtconn: Test OK')
             if VERBOSE:
                 pprint(res)
                 print('')
